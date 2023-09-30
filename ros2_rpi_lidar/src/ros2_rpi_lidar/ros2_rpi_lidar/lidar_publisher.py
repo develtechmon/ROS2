f693925c23baa74562d83448d7d76dd5bcc5eaa9
develtechmon <lukasjohnnytechmon@gmail.com>
2023-09-30 19:36:46 +0800

    add node for lidar publisher

--- ros2_rpi_lidar/src/ros2_rpi_lidar/ros2_rpi_lidar/lidar_publisher.py
+++ ros2_rpi_lidar/src/ros2_rpi_lidar/ros2_rpi_lidar/lidar_publisher.py
@@ -1,20 +1,36 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
-from vl53lox import *
+
+## ros2 interface show sensor_msgs/msg/Range
+from sensor_msgs.msg import Range
+from lidar_vl53lox import Sonar
+
 class LidarPublisher(Node):
     def __init(self):
         super().__init__('lidar_publisher')
         topic_name = "/dkcar/lidar"
-        self.publishers_ = self.create_publisher(String, topic_name, 10)
-        timer_period = 0.5
-        self.timers = self.create_timer(timer_period, self.timer_callback)
-        self.i=0
+        self.publishers_ = self.create_publisher(Range, topic_name, 5)
+        
+        message = Range()
+        self._message = message
+
+        self.scan()
+        
+        # timer_period = 0.5
+        # self.timers = self.create_timer(timer_period, self.timer_callback)
+        # self.i=0
     
-    def timer_callback(self):
-        msg = String()
-        msg.data = "Hello World"
-        self.publishers_.publish(msg)
+    # def timer_callback(self):
+    #     msg = String()
+    #     msg.data = "Hello World"
+    #     self.publishers_.publish(msg)
+    
+    def scan(self):
+        message = Sonar()
+        while True:
+            range_cm = Sonar.read()
+            self._message.range = range_cm * 0.01
+            self.publishers_(self._message)
         
 def main(args=None):
     rclpy.init(args=args)
--- ros2_rpi_lidar/src/ros2_rpi_lidar/ros2_rpi_lidar/lidar_vl53lox.py
+++ ros2_rpi_lidar/src/ros2_rpi_lidar/ros2_rpi_lidar/lidar_vl53lox.py
@@ -16,7 +16,6 @@ class Sonar():
         with self.vl53.continuous_mode():
             while True:
                 time.sleep(0.1)
-                
                 return self.vl53.range
             
 if __name__ == "__main__":
