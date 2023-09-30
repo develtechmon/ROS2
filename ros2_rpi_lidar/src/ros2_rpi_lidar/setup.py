a3ec0795e8b188186f89aefa27f5f7675df3b983
develtechmon <lukasjohnnytechmon@gmail.com>
2023-09-30 19:39:36 +0800

    add node for lidar publisher

--- ros2_rpi_lidar/src/ros2_rpi_lidar/setup.py
+++ ros2_rpi_lidar/src/ros2_rpi_lidar/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'lidar_publisher = ros2_rpi_lidar.lidar_publisher:main()'
         ],
     },
 )
