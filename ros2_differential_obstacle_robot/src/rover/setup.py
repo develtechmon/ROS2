e53b29af605d7188f29cb13f0d19acb3afeaf3ff
develtechmon <lukasjohnnytechmon@gmail.com>
2023-10-26 18:20:10 +0800

    + added new project

--- ros2_differential_obstacle_robot/src/rover/setup.py
+++ ros2_differential_obstacle_robot/src/rover/setup.py
@@ -13,7 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*')),
-        (os.path.join('share', package_name, 'description'), glob('description/*'))
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*'))
 
     ],
     install_requires=['setuptools'],
