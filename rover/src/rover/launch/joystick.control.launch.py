9e3858dacecaf74e53573d5e3d5b06df9cec0391
develtechmon <lukasjohnnytechmon@gmail.com>
2023-10-28 03:54:36 +0800

    +

--- rover/src/rover/launch/joystick.control.launch.py
+++ rover/src/rover/launch/joystick.control.launch.py
@@ -1,17 +1,59 @@
+
 import launch
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import DeclareLaunchArgument
+from launch.actions import LogInfo
+from launch.actions import LaunchProcess
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     return LaunchDescription([
-        # Launch the 'teleop-twist-joy' using ExecuteProcess
-        ExecuteProcess(
+        # Launch the 'teleop-twist-joy' node with 'teleop-launch.py'
+        LaunchProcess(
             cmd=['ros2', 'launch', 'teleop_twist_joy', 'teleop-launch.py'],
             output='screen',
+            name='teleop_twist_joy_node',
         ),
-        # Set the parameter using ExecuteProcess
-        ExecuteProcess(
+        # Wait for a moment before setting the parameter (adjust as needed)
+        LogInfo(
+            condition=launch.conditions.LaunchConfiguration('verbose'),
+            value='Waiting for a moment before setting the parameter...',
+        ),
+        # Set the 'require_enable_button' parameter to 'false'
+        LaunchProcess(
             cmd=['ros2', 'param', 'set', '/teleop_twist_joy_node/require_enable_button', 'false'],
             output='screen',
+            name='set_param',
+        ),
+        DeclareLaunchArgument(
+            'verbose',
+            default_value='false',
+            description='Enable verbose output'
+        ),
+        # You can adjust the sleep duration to suit your needs
+        LaunchProcess(
+            cmd=['sleep', '2'],
+            output='screen',
+            name='wait_before_setting_param',
+            condition=launch.conditions.UnlessLaunchConfiguration('verbose'),
         ),
     ])
+
+
+# import launch
+# from launch import LaunchDescription
+# from launch.actions import ExecuteProcess
+
+# def generate_launch_description():
+#     return LaunchDescription([
+#         # Launch the 'teleop-twist-joy' using ExecuteProcess
+#         ExecuteProcess(
+#             cmd=['ros2', 'launch', 'teleop_twist_joy', 'teleop-launch.py'],
+#             output='screen',
+#         ),
+#         # Set the parameter using ExecuteProcess
+#         ExecuteProcess(
+#             cmd=['ros2', 'param', 'set', '/teleop_twist_joy_node/require_enable_button', 'false'],
+#             output='screen',
+#         ),
+#     ])
