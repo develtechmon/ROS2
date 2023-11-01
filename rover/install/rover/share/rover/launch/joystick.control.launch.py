82385ecf9353c172738ac5e91c2a2cd7fe2264bf
develtechmon <lukasjohnnytechmon@gmail.com>
2023-11-01 18:58:17 +0800

    +edit joystick time

--- rover/install/rover/share/rover/launch/joystick.control.launch.py
+++ rover/install/rover/share/rover/launch/joystick.control.launch.py
@@ -1,20 +1,25 @@
 import launch
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, TimerAction
 from time import sleep
 def generate_launch_description():
+       
+    set_param = ExecuteProcess(
+        cmd=['ros2', 'param', 'set', '/teleop_twist_joy_node','require_enable_button', 'false'],
+        output='screen',
+    ),
     
+    delay_execute_process = TimerAction(period=3.0, actions=[set_param])
     return LaunchDescription([
 
         #Set the parameter using ExecuteProcess
-        ExecuteProcess(
-            cmd=['ros2', 'param', 'set', '/teleop_twist_joy_node','require_enable_button', 'false'],
-            output='screen',
-        ),
+    
         # Launch the 'teleop-twist-joy' using ExecuteProcess
         ExecuteProcess(
             cmd=['ros2', 'launch', 'teleop_twist_joy', 'teleop-launch.py'],
             output='screen',
         ),
 
+
     ])
+
