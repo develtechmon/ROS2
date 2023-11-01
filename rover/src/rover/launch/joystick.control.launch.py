5f1bdc367babc9144562bde73e42b66ad4e766b7
develtechmon <lukasjohnnytechmon@gmail.com>
2023-11-01 20:46:15 +0800

    +added working teleop

--- rover/src/rover/launch/joystick.control.launch.py
+++ rover/src/rover/launch/joystick.control.launch.py
@@ -1,25 +1,27 @@
 import launch
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, TimerAction
-from time import sleep
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument,SetLaunchConfiguration
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
+import os
+
 def generate_launch_description():
-       
-    set_param = ExecuteProcess(
-        cmd=['ros2', 'param', 'set', '/teleop_twist_joy_node','require_enable_button', 'false'],
-        output='screen',
-    ),
-    
-    delay_execute_process = TimerAction(period=3.0, actions=[set_param])
+    joystick = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory('teleop_twist_joy'),'launch','teleop-launch.py'
+            )]),
+    )
+
     return LaunchDescription([
+        joystick,
 
-        #Set the parameter using ExecuteProcess
-        
-        # Launch the 'teleop-twist-joy' using ExecuteProcess
         ExecuteProcess(
-            cmd=['ros2', 'launch', 'teleop_twist_joy', 'teleop-launch.py'],
-            output='screen',
-        ),
-
-
+         cmd=['ros2', 'param', 'set', '/teleop_twist_joy_node','require_enable_button', 'false'],
+         output='screen',
+         ),  
     ])
 
