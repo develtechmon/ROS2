c0022c3b3fccbfa869c451f55a317d9f2ef7b555
develtechmon <lukasjohnnytechmon@gmail.com>
2023-11-02 14:12:15 +0800

    +correct the bug

--- ros2_rpi_motor/src/ros2_rpi_motor/ros2_rpi_motor/motor_l298n.py
+++ ros2_rpi_motor/src/ros2_rpi_motor/ros2_rpi_motor/motor_l298n.py
@@ -8,32 +8,35 @@ class MotorController(Node):
         super().__init__('motor_controller')
         self.cmd_vel_sub = self.create_subscription(Twist, '/cmd_vel', self.cmd_vel_callback, 10)
         self.setup_gpio_pins()
+        self.left_motor_pins = None
+        self.right_motor_pins = None
+
 
     def setup_gpio_pins(self):
         GPIO.setmode(GPIO.BCM)
 
         # Define GPIO pins for left and right wheels
-        left_motor_pins = {
+        self.left_motor_pins = {
             'enable': 17,
             'input1': 22,
             'input2': 27
         }
 
-        right_motor_pins = {
+        self.right_motor_pins = {
             'enable': 25,
             'input1': 24,
             'input2': 23
         }
 
-        for pin in left_motor_pins.values():
+        for pin in self.left_motor_pins.values():
             GPIO.setup(pin, GPIO.OUT)
 
-        for pin in right_motor_pins.values():
+        for pin in self.right_motor_pins.values():
             GPIO.setup(pin, GPIO.OUT)
 
         # Set PWM for the enable pins
-        self.left_motor_pwm = GPIO.PWM(left_motor_pins['enable'], 100)
-        self.right_motor_pwm = GPIO.PWM(right_motor_pins['enable'], 100)
+        self.left_motor_pwm = GPIO.PWM(self.left_motor_pins['enable'], 100)
+        self.right_motor_pwm = GPIO.PWM(self.right_motor_pins['enable'], 100)
         self.left_motor_pwm.start(0)
         self.right_motor_pwm.start(0)
 
@@ -54,10 +57,10 @@ class MotorController(Node):
         right_direction = GPIO.HIGH if right_speed >= 0 else GPIO.LOW
 
         # Set motor directions
-        GPIO.output(left_motor_pins['input1'], left_direction)
-        GPIO.output(left_motor_pins['input2'], not left_direction)
-        GPIO.output(right_motor_pins['input1'], right_direction)
-        GPIO.output(right_motor_pins['input2'], not right_direction)
+        GPIO.output(self.left_motor_pins['input1'], left_direction)
+        GPIO.output(self.left_motor_pins['input2'], not left_direction)
+        GPIO.output(self.right_motor_pins['input1'], right_direction)
+        GPIO.output(self.right_motor_pins['input2'], not right_direction)
 
         # Set motor speed via PWM
         self.left_motor_pwm.ChangeDutyCycle(left_duty_cycle)
