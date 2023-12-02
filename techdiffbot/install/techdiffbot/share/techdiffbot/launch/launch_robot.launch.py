8e7ee827b396b873bb626bc763dcb6540f6320b0
develtechmon <lukasjohnnytechmon@gmail.com>
2023-12-03 03:44:09 +0800

    +modify urdf for ros2 control

--- techdiffbot/install/techdiffbot/share/techdiffbot/launch/launch_robot.launch.py
+++ techdiffbot/install/techdiffbot/share/techdiffbot/launch/launch_robot.launch.py
@@ -14,6 +14,7 @@ import os
 def generate_launch_description():
     package_name = 'techdiffbot'
 
+    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
     rsp = IncludeLaunchDescription(
             PythonLaunchDescriptionSource([os.path.join(
                 get_package_share_directory(package_name),'launch','rsp.launch.py'
@@ -21,7 +22,6 @@ def generate_launch_description():
     )
     
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
-    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','real_robot_params.yaml')
 
     controller_manager = Node(
         package="controller_manager",
