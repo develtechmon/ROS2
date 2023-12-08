253889598807834f775c84c6897018135aac168f
Captluke2328 <125783320+develtechmon@users.noreply.github.com>
2023-12-08 08:49:59 +0800

    +added ros2 arduino hardware interface

---
+++ ros_Arduino/ros_arduino_bridge/ROSArduinoBridge/motor_driver.h
@@ -0,0 +1,23 @@
+/***************************************************************
+   Motor driver function definitions - by James Nugen
+   *************************************************************/
+
+#ifdef L298_MOTOR_DRIVER
+  #define RIGHT_MOTOR_BACKWARD 5 //12 
+  #define LEFT_MOTOR_BACKWARD  6
+  #define RIGHT_MOTOR_FORWARD  9
+  #define LEFT_MOTOR_FORWARD   10
+  #define RIGHT_MOTOR_ENABLE 12 //5 
+  #define LEFT_MOTOR_ENABLE 11 //13
+
+//  #define LEFT_MOTOR_BACKWARD 5 //12 
+//  #define RIGHT_MOTOR_BACKWARD  6
+//  #define LEFT_MOTOR_FORWARD  9
+//  #define RIGHT_MOTOR_FORWARD   10
+//  #define LEFT_MOTOR_ENABLE 12 //5 
+//  #define RIGHT_MOTOR_ENABLE 11 //13
+#endif
+
+void initMotorController();
+void setMotorSpeed(int i, int spd);
+void setMotorSpeeds(int leftSpeed, int rightSpeed);
